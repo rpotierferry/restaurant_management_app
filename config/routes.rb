ea14446febc0cb7d6e026d30353aedfa502f5ae0
Romain Potier-Ferry <romainpotierferry@gmail.com>
2024-03-25 22:10:28 +0100

    add user management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'restaurants#index'
   resources :restaurants, only: %w[index new show create] do
     resources :reviews, only: 'create'
