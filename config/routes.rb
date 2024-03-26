5d17a1a749fd868f66c35c50fb9bdfe69291ea8f
Romain Potier-Ferry <romainpotierferry@gmail.com>
2024-03-26 14:31:46 +0100

    add some basic style to show restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'restaurants#index'
   resources :restaurants, only: %w[index new show create] do
-    resources :reviews, only: 'create'
+    resources :reviews, only: %w[create index]
   end
 end
